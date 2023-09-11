751b688984b88db76f2e74d3d36bf99cc0f49f44
Brandonchriss <bkretser.2020@sis.smu.edu.sg>
2023-09-11 15:30:51 +0800

    change typed words

--- src/components/Typed.js
+++ src/components/Typed.js
@@ -4,7 +4,7 @@ import Typed from 'typed.js';
 const TypedText = () => {
     useEffect(() => {
         const options = {
-            strings: ["a Full Stack Software Engineer", "a Data Enthusiast", "a dude that stumbled upon coding in 2015 and stuck with it"],
+            strings: ["an Aspiring Software Engineer", "a Data Enthusiast", "a dude that stumbled upon coding in 2015 and stuck with it"],
             typeSpeed: 50,
             backSpeed: 30,
             loop: false
